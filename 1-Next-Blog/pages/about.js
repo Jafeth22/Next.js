ff32c538008c62e2913ed623290dd052ae1bf6f1
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-09-30 17:20:08 -0600

    Create head section

--- 1-Next-Blog/pages/about.js
+++ 1-Next-Blog/pages/about.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 
 function AboutPage() {
     console.log("About")
     return (
         <>
+            <Head>
+                <title>Home</title>
+                <meta name="description" value="This is the Home page" />
+            </Head>
             <main>
                 <h1>About Page</h1>
             </main>
--- 1-Next-Blog/pages/index.js
+++ 1-Next-Blog/pages/index.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 
 function HomePage() {
     console.log("Home")
     return (
         <>
+            <Head>
+                <title>About</title>
+                <meta name="description" value="This is the About page" />
+            </Head>
             <main>
                 <h1>Home Page</h1>
             </main>
