6cd1b5b743a3b0d9e906c5598b215fc1ac7bf7a3
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-10-01 14:17:55 -0600

    "Dinamic" Page

---
+++ 1-Next-Blog/content/posts/second-post.md
@@ -0,0 +1,6 @@
+---
+title: Second Post
+date: "2021-10-01"
+---
+
+This is my Second post, written in Markdown.
--- 1-Next-Blog/pages/posts/[slug].js
+++ 1-Next-Blog/pages/posts/[slug].js
@@ -2,13 +2,22 @@
 import Head from 'next/head';
 import { getPost } from '../../lib/post';
 
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { slug: 'first-post' } },
+            { params: { slug: 'second-post' } }
+        ],
+        fallback: false //This in case any path match, it won't show 404 page
+    }
+}
 
 /**
  * This function runs only on SERVER side, for that reason we don't see the message on the web console, but it does on the SERVER console
  */
-export async function getStaticProps() {
-    console.log('[FirstPostPage] getStaticProps()')
-    const post = await getPost('first-post');
+export async function getStaticProps({ params: { slug } }) {
+    console.log('[PostPage] getStaticProps(): ', slug)
+    const post = await getPost(slug);
 
     return {
         props: { post }
@@ -18,9 +27,9 @@ export async function getStaticProps() {
 /**
  * This function runs only on CLIENT side
  */
-function FirstPostPage({post}) {
-    console.log('[FirstPostPage] render', post);
-    return(
+function PostPage({ post }) {
+    console.log('[PostPage] render', post);
+    return (
         <>
             <Head>
                 <title>{post.title} - My blog</title>
@@ -29,7 +38,7 @@ function FirstPostPage({post}) {
                 <h1>{post.title}</h1>
                 <h5>{post.date}</h5>
                 {/* dangerouslySetInnerHTML = This is to improve the security and not insert the HTML directly*/}
-                <article dangerouslySetInnerHTML={{__html: post.body}} />
+                <article dangerouslySetInnerHTML={{ __html: post.body }} />
                 {/* <p>
                     {post.body}
                 </p> */}
@@ -38,4 +47,4 @@ function FirstPostPage({post}) {
     );
 }
 
-export default FirstPostPage;
+export default PostPage;
