cd9d7c2376a3a24a6889456564b6613e48e24319
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-10-01 10:08:18 -0600

    Reading local file

--- 1-Next-Blog/pages/posts/first-post.js
+++ 1-Next-Blog/pages/posts/first-post.js
@@ -1,18 +1,24 @@
+import { readFile } from 'fs/promises'
 import Head from 'next/head'
 
+/**
+ * This function runs only on SERVER side, for that reason we don't see the message on the web console, but it does on the SERVER console
+ */
 export async function getStaticProps() {
+    console.log('[FirstPostPage] getStaticProps()')
+    const data = await readFile('content/posts/first-post.json', 'utf8')
+    const post = JSON.parse(data);
+
     return {
-        props: {
-            post: {
-                title: "First Post",
-                body: "Body Content"
-            }
-        }
+        props: { post }
     };
 }
 
+/**
+ * This function runs only on CLIENT side
+ */
 function FirstPostPage({post}) {
-    console.log('FirstPostPage', post);
+    console.log('[FirstPostPage] render', post);
     return(
         <>
             <Head>
