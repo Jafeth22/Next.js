9dd338e55381f4b5963d8a036b1da27ca048d6ef
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-10-05 15:08:57 -0600

    Add images and css style

--- 2-Next-shop/pages/index.js
+++ 2-Next-shop/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import ProductCard from '../components/ProductCard';
 import Title from '../components/Title';
 import { getProducts } from '../lib/products';
 
@@ -7,7 +8,7 @@ import { getProducts } from '../lib/products';
 export async function getStaticProps() {
   console.log('[HomePage] getStaticProps()');
   const products = await getProducts();
-  return { 
+  return {
     props: { products },
     revalidate: parseInt(process.env.REVALIDATE_SECONDS)
   }
@@ -22,12 +23,10 @@ export default function HomePage({ products }) {
       </Head>
       <main className="px-6 py-5" >
         <Title>Next Shop</Title>
-        <ul>
+        <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4" >
           {products.map((product) => (
             <li key={product.id} >
-              <Link href={`/products/${product.id}`}>
-                {product.title}
-              </Link>
+              <ProductCard product={product} />
             </li>
           ))}
         </ul>
