ca70628806ae3ceed51c736f4b0432dd3b71ad47
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-10-06 09:05:22 -0600

    Creating sign-in page

---
+++ 2-Next-shop/components/Input.js
@@ -0,0 +1,6 @@
+function Input({type}) {
+    return (
+        <input type={type} className="border rounded px-3 py-1 w-80" />
+    );
+}
+export default Input;
--- 2-Next-shop/pages/sign-in.js
+++ 2-Next-shop/pages/sign-in.js
@@ -1,10 +1,22 @@
 import Page from "../components/Page";
-
+import Input from "../components/Input";
+import Field from "../components/Field";
+import Button from "../components/Button";
 
 function SingInPage(){
     return (
         <Page title="Sing In">
-
+            <form>
+                <Field label="Email">
+                    <Input type="email" />
+                </Field>
+                <Field label="Password">
+                    <Input type="password" />
+                </Field>
+                <Button type="submit">
+                    Sign in
+                </Button>
+            </form>
         </Page>
     );
 }
