34eb4f5e51cf58099c80d51f92a5f1345ed57b1c
Jafeth Vargas <33306997+Jafeth22@users.noreply.github.com>
2022-09-23 22:00:52 -0600

    add comparative note about ssr and ssg/isg

--- pre_generation/pages/books/[id].js
+++ pre_generation/pages/books/[id].js
@@ -3,21 +3,30 @@ import { useRouter } from 'next/router'
 import { getBooks, getBooksFromId } from '../../utils/api-ultils';
 import Link from 'next/link';
 
-export async function getStaticPaths() {
-    const books = await getBooks();
-    const paths = books.map((book) => ({params: { id: book.id }}));
-    return {
-        paths,
-        fallback: false, // false = This is when the page doesn't find the route, it will return 404
-                        // true = It will try to find the page
-    };
-}
+// // This is to work with SSG or ISG ---------------------------------------------------------------
+// export async function getStaticPaths() {
+//     const books = await getBooks();
+//     const paths = books.map((book) => ({params: { id: book.id }}));
+//     return {
+//         paths,
+//         fallback: false, // false = This is when the page doesn't find the route, it will return 404
+//                         // true = It will try to find the page
+//     };
+// }
 
-/**
- * getStaticProps has default the context object inside it,
- * and it has all the features of this function
- */
-export async function getStaticProps({params}) {
+// /**
+//  * getStaticProps has default the context object inside it,
+//  * and it has all the features of this function
+//  */
+// export async function getStaticProps({params}) {
+//     const book = await getBooksFromId(params.id);
+//     return {
+//         props: { book }
+//     }
+// }
+
+// // This is to work with SSR ----------------------------------------------------------------
+export async function getServerSideProps({ params }) {
     const book = await getBooksFromId(params.id);
     return {
         props: { book }
